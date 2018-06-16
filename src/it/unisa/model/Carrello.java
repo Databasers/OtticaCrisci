7b8b11675c31f665c75f2525e7e4bcc5f5b74da2
00Joshua <luigicrisci1997@gmail.com>
2018-06-16 18:33:36 +0200

    Aggiunto carrello e ProductModel Tutta roba copiata dal prof perchè mi scocciavo e funziona benissimo

---
+++ src/it/unisa/model/Carrello.java
@@ -0,0 +1,32 @@
+package it.unisa.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Carrello<T> implements Serializable{
+	List<T> list;
+	
+	public Carrello(){
+		list= new ArrayList<T>();
+	}
+	
+	public void addElement(T elem)
+	{
+		list.add(elem);
+	}
+	
+	public void delete(T elem)
+	{
+		for(T e: list){
+			if(e.equals(elem))
+				list.remove(elem);
+		}
+	}
+	
+	public List<T> getList()
+	{
+		return list;
+	}
+
+}
