2133c57dd482c5701d83a4cf4aeedf4b6be3b2a9
00Joshua <luigicrisci1997@gmail.com>
2018-07-04 22:29:00 +0200

    Aggiunta logica per aggiunta di un certificato

--- WebContent/HTML/Utente.jsp
+++ WebContent/HTML/Utente.jsp
@@ -3,12 +3,40 @@
 <head>
 <meta charset="ISO-8859-1">
 <title>Insert title here</title>
+
+<script type="text/javascript">
+
+	function hideCertificato(){
+		var form=document.getElementById("certificato");
+		//Qui andrebbe nascosto il form di input
+	}
+</script>
+
 </head>
 <body>
 
 	<!-- 
 		Manco questa abbiamo visto per motivi randomici, si accettano proposte
 	 -->
+	 
+	 <%
+	 	Boolean aggiunto =(Boolean)request.getAttribute("certificatoInserito");
+	 	if(aggiunto!=null)
+	 		if(aggiunto){
+	 			%>
+	 			<h3>Certificato aggiunto</h3>
+	 		<% }
+	 		else{
+	 			%>
+	 			<h3>Certificatoo non aggiunto</h3>
+	 		<% }
+	 	%>
+
+
+	<form id="certificato" action="http://localhost:8080/OtticaCrisci/GestioneUtente?action=addCertificato" method="post" enctype="multipart/form-data">
+		<input type="file" accept=".img,.pdf" name="certificato">
+		<input type="submit">
+	</form>
 
 </body>
 </html>
--- src/servlet/GestioneUtente.java
+++ src/servlet/GestioneUtente.java
@@ -1,36 +1,81 @@
 package servlet;
 
+import java.io.File;
 import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.sql.SQLException;
+import java.util.Collection;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+import org.apache.catalina.connector.Request;
+
+import bean.Certificato;
+import bean.Cliente;
+import bean.SessioneUtente;
+import managerBean.CertificatoManager;
+import managerBean.ClienteManager;
 /**
  * Servlet implementation class GestioneUtente
  */
 @WebServlet("/GestioneUtente")
+@MultipartConfig(fileSizeThreshold= 1024*1024*2, maxFileSize=1024*1024*10, maxRequestSize=1024*1024*50 )
 public class GestioneUtente extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+	private CertificatoManager certificatoManager;
+    private ClienteManager clienteManager;
+    private Cliente cliente;
+    
     public GestioneUtente() {
         super();
-        // TODO Auto-generated constructor stub
+        certificatoManager=new CertificatoManager();
+        clienteManager= new ClienteManager();
     }
 
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		SessioneUtente su=(SessioneUtente) request.getSession().getAttribute("su");
+		try {
+			cliente=clienteManager.doRetrieveByKey(su.getcF()); //recupero il cliente che riguarda questa chiamata
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		String action=(String)request.getParameter("action");
 		if(action.equalsIgnoreCase("modificaPassword"))
 			doModificaPassword(request,response);
-		
+		if(action.equalsIgnoreCase("addCertificato"))
+			try {
+				doAddCertificato(request,response);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		
 	}
+
+	private void doAddCertificato(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException, SQLException {
+		String saveDir="C:\\Users\\Antonio\\eclipse--EEworkspace-servlet\\OtticaCrisci\\Data\\Certificati";
+		for(Part certificato: request.getParts()) //tanto lo fa una sola volta
+		{
+			String filename= certificato.getSubmittedFileName();
+			saveDir+=File.separator+filename;
+			if(filename!=null && !filename.equals("")) {
+				certificato.write(saveDir); 
+				System.out.println("Directory di salvataggio: " + saveDir);
+				certificatoManager.doSave(new Certificato( cliente.getcF(),saveDir,false));
+				request.setAttribute("certificatoInserito", true);
+			}
+			else
+				request.setAttribute("certificatoInserito", true);
+		}
+		RequestDispatcher x= getServletContext().getRequestDispatcher("/HTML/Utente.jsp");
+		x.forward(request, response);
+	}
+
+
 
 	private void doModificaPassword(HttpServletRequest request, HttpServletResponse response) {
 		
